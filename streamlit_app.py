bdb9db92a8269fb49906314f6107f394df6855fb
Mandip Chaudhary <mandip810@gmail.com>
2024-09-30 14:12:03 +0545

    feat: remove inference API with hb models

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import os
 from dotenv import load_dotenv 
-import requests
-# import torch
+# import requests
+import torch
 import streamlit as st
-# from transformers import pipeline
+from transformers import pipeline
 from huggingface_hub import InferenceClient
 from datetime import datetime
+from transformers import VitsModel, AutoTokenizer
+from functools import lru_cache
+from scipy.io import wavfile
+import numpy as np
 
 load_dotenv()
 
@@ -17,55 +21,96 @@ client = InferenceClient(
 
 # replace local model with huggingface inference client
 # # image to text model
-# def image2text(url):
-#     image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base", max_new_tokens=1000, device=1)
-#     text = image_to_text(url)[0]['generated_text']
+def image2text(url):
+    # model="Salesforce/blip-image-captioning-base"
+    model="nlpconnect/vit-gpt2-image-captioning"
+    image_to_text = pipeline("image-to-text", model, max_new_tokens=10, device=1)
+    caption = image_to_text(url)
+    print(caption)
+    text = caption[0]['generated_text']
 
-#     return text
+    return text
 
 # image to text model
-def image2text(filename):
-    print(filename)
-    API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-base"
-    headers = {"Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"}
-    with open(filename, "rb") as f:
-        data = f.read()
-    response = requests.post(API_URL, headers=headers, data=data).json()
-    print("\n=====================================================\n")
-    print(response)
-    print("\n=====================================================\n")
-    return response[0]["generated_text"]
+# def image2text(filename):
+#     print(filename)
+#     API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-base"
+#     headers = {"Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"}
+#     with open(filename, "rb") as f:
+#         data = f.read()
+#     response = requests.post(API_URL, headers=headers, data=data).json()
+#     print("\n=====================================================\n")
+#     print(response)
+#     print("\n=====================================================\n")
+#     return response[0]["generated_text"]
 
-# llm
-def generate_story(scenario):
+# caption to story
+@lru_cache(maxsize=100)
+def caption2story(caption):
     template = """
         You are a storyteller;
         You can generate a story based on a simple narrative under 100 words with the following context:
-        CONTEXT: {scenario}
-    """.format(scenario=scenario)
+        CONTEXT: {caption}
+    """.format(caption=caption)
+    model = "Qwen/Qwen2.5-0.5B"
+    caption_to_story = pipeline("text-generation", model, max_new_tokens=100, device=1)
+    story = caption_to_story(template)
+    print(story)
+    return story[0]['generated_text']
 
-    # Inference client load chat completion
-    response = client.chat_completion(
-        messages=[{"role": "user", "content": template}],
-        max_tokens=100,
-        # stream=True,
-    )
+# llm
+# def generate_story(scenario):
+#     template = """
+#         You are a storyteller;
+#         You can generate a story based on a simple narrative under 100 words with the following context:
+#         CONTEXT: {scenario}
+#     """.format(scenario=scenario)
 
-    return response.choices[0].message.content
+#     # Inference client load chat completion
+#     response = client.chat_completion(
+#         messages=[{"role": "user", "content": template}],
+#         max_tokens=100,
+#         # stream=True,
+#     )
+
+#     return response.choices[0].message.content
 
 
 # text to speech model
-def text2speech(text, filename):
-    headers = {"Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"}
-    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
-    payload = {
-        "inputs": text
-    }
-    response = requests.post(API_URL, headers=headers, json=payload)
+# def text2speech(text, filename):
+#     headers = {"Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"}
+#     API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+#     payload = {
+#         "inputs": text
+#     }
+#     response = requests.post(API_URL, headers=headers, json=payload)
+#     filepath = 'output.flac' if filename is None else filename
+    
+#     with open(filepath, "wb") as f:
+#         f.write(response.content)
+
+#     return filepath
+
+# def text2speech(text, filename):
+#     model = "myshell-ai/MeloTTS-English"
+#     text_to_speech = pipeline("text2audio", model, max_new_tokens=100, device=1)
+#     response = text_to_speech(text)
+#     filepath = 'output.flac' if filename is None else filename
+    
+#     with open(filepath, "wb") as f:
+#         f.write(response.content)
+
+#     return filepath
+
+def text2speech_fb(text, filename):
+    text_to_speech = pipeline("text-to-speech", model="facebook/mms-tts-eng")
+    response = text_to_speech(text)
     filepath = 'output.flac' if filename is None else filename
+
+    print(response)
     
     with open(filepath, "wb") as f:
-        f.write(response.content)
+        f.write(response['audio'])
 
     return filepath
 
@@ -83,16 +128,17 @@ def main():
             st.image(uploaded_file, caption="Uploaded image..",
                 use_column_width=True)
             scenerio = image2text(uploaded_file.name)
-            story = generate_story(scenario=scenerio)
+            story = caption2story(caption=scenerio)
             filename = f"{datetime.now()}-story.flac"
-            filepath = text2speech(story, filename=filename)
+            filepath = text2speech_fb(story, filename=filename)
 
             with st.expander("scenerio"):
                 st.write(scenerio)
             with st.expander("story"):
                 st.write(story)
 
-            st.audio(filepath)
+            if filepath is not None:
+                st.audio(filepath)
 
 
 if __name__ == "__main__":
